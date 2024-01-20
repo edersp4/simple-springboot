71a88e74ee728de36e21e734e5fead3b9b768782
edsilva <edsilvasp@hotmail.com>
2024-01-20 19:46:49 -0300

    simple-springboot-7 Logging: Introducing logging statements in the application code
    -Created logback.xml to set configuration about the logs
    - Created logs into application with level debug , info and error
    
    close #7

--- src/main/java/com/simple/simplespringboot/controller/SimpleController.java
+++ src/main/java/com/simple/simplespringboot/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package com.simple.simplespringboot.controller;
 
 import com.simple.simplespringboot.exceptions.RequiredException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,25 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
 
     @GetMapping("/greet")
     public String greet() {
+        log.info("Hello, welcome to Spring Boot REST API!");
         return "Hello, welcome to Spring Boot REST API!";
     }
 
     @GetMapping("/echo")
     public String echo(@RequestParam String message) {
+        log.info("Echoing message: {}", message);
         return message;
     }
 
 
     @GetMapping("/square")
     public int square(@RequestParam(required = false) Integer number) {
-        if (Objects.isNull(number))
+        if (Objects.isNull(number)) {
+            log.error("Number cannot be null");
             throw new RequiredException("Number cannot be null");
-        return number * number;
+        }
+        final var result = number * number;
+        log.info("Square of {} is {}", number, result);
+        return result;
     }
 }
--- src/main/java/com/simple/simplespringboot/services/UserService.java
+++ src/main/java/com/simple/simplespringboot/services/UserService.java
@@ -3,24 +3,29 @@ package com.simple.simplespringboot.services;
 import com.simple.simplespringboot.model.User;
 import com.simple.simplespringboot.model.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
     public List<User> getAll() {
+        log.debug("Getting all users");
         return userRepository.findAll();
     }
 
     public User save(User user) {
+        log.debug("Saving user: {}", user);
         return userRepository.save(user);
     }
 
     public void delete(Long id) {
+        log.debug("Deleting user with id: {}", id);
         userRepository.deleteById(id);
     }
 }
